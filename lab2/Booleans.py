acc03a8dd1ed1593e68d3c6594ddff41e43c6bf9
AltynbekMaksat <altynbekmax@gmail.com>
2024-01-25 16:48:41 +0600

    a

--- lab2/Booleans.py
+++ lab2/Booleans.py
@@ -1,1 +1,14 @@
-print("hello")
+# EX 1
+print(10 > 9) #output : True
+
+#EX 2
+print(10 == 9 ) #output : False
+
+#EX 3 
+print(10 < 9 ) # output : False
+
+#EX 4
+print(bool("abc")) #output : True
+
+#EX 5
+print(bool(0)) #output: False
