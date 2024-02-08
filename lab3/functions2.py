d63105ea7453de59cd90e317114b961ef9c6ef32
AltynbekMaksat <altynbekmax@gmail.com>
2024-02-08 15:05:53 +0600

    a

--- lab3/functions2.py
+++ lab3/functions2.py
@@ -77,11 +77,59 @@ movies = [
 "category": "Romance"
 }
 ]
+# # ex 1
+# def score(x):
+#     if movies[x]["imdb"] > 5.5:
+#         return True
+#     else:
+#         return False
+# num = int(input())
+# print(score(num))
 
-def score(x):
-    if movies[x]["imdb"] > 5.5:
-        return True
-    else:
-        return False
-num = int(input())
-print(score(num))
+# # ex 2
+
+# def List():
+#     i = 0
+#     lis1 = []
+#     while i < len(movies):
+#         if int(movies[i]["imdb"]) > 5.5:
+#             lis1.append(movies[i]["name"])
+#         i +=1
+#     print(lis1)
+# List() 
+
+# # ex 3
+
+# def ctr(a):
+#     List = []
+#     for x in movies:
+#         if x["category"]==a:
+#             List.append(x["name"])
+#     print(List)
+
+# a = str(input())
+# ctr(a)
+
+# # ex 4
+
+# def avr():
+#     sum = 0 
+#     for x in movies:
+#         sum+=x['imdb']
+#     print(sum/len(movies))
+
+# avr()
+
+# # ex 5
+
+# def avr(a):
+#     sum = 0
+#     i = 0 
+#     for x in movies:
+#         if x["category"]== a:
+#             sum+=x['imdb']
+#             i+=1
+#     print(sum/i)
+    
+# a=str(input())
+# avr(a)
